ea22e0c79e0b457e6a5a52f8372dd2a7667be454
Deepansh Vishwakarma <117040777+codebydeep@users.noreply.github.com>
2024-07-14 14:59:33 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // We are using NodeJS, not browser
+
+console.log(3 
+    + 3) // Code readability should be high
+ console.log("Really")
+
+ // DataTypes
+
+ let name = "Deep"
+ let age = 18
+ let isLoggedIn = false
+ let state;
+
+ // Primitives
+ // number => 2 to power 53
+ // bigint 
+ // string => ""
+ // boolean => true / false
+ // null => standalone value, also a object
+ // undefined => value not assigned to variable
+ // symbol => unique
+
+ // Object
+
+ console.log(typeof "hello") // string
+ console.log(typeof undefined) // undefined
+ console.log(typeof null) // object
+
